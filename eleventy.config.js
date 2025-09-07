ed4ac4e50376e8bc21911802937240b67a029f3a
Florian Overfelt <joverfelt@meso-scale.com>
2025-09-07 16:36:11 -0400

    Reset everything.

--- eleventy.config.js
+++
@@ -1,5 +0,0 @@
-export default function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("**/*.css");
-    eleventyConfig.addPassthroughCopy("**/*.ico");
-    eleventyConfig.addPassthroughCopy("**/*.svg");
-};
